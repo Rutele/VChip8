a774ffc08a7b7eccf2ea9201446e2deb095e6fbc
Michal Rucinski <rucinskimichal7@gmail.com>
2025-09-07 16:16:51 +0200

    Revised ALU implementation

--- rtl/datapath/alu/alu.vhd
+++ rtl/datapath/alu/alu.vhd
@@ -6,50 +6,50 @@ use work.chip8_types_pkg.all;
 
 entity CHIP8_ALU is
     port(
-        i_VX, i_VY: in std_logic_vector(CHIP8_WORD_SIZE-1 downto 0);
-        i_ALUCtrl: in std_logic_vector(CHIP8_ALU_CTRL_SIZE-1 downto 0);
-        o_ALUData: out std_logic_vector(CHIP8_WORD_SIZE-1 downto 0);
-        o_CB: out std_logic    -- Carry or borrow flag
+        i_VX, i_VY:     in std_logic_vector(CHIP8_WORD_SIZE-1 downto 0);
+        i_ALUCtrl:      in chip8_alu_op_t;
+        o_ALUResult:    out std_logic_vector(CHIP8_WORD_SIZE-1 downto 0);
+        o_UniF:           out std_logic    -- Universal flag interpreted based on the instruction
     );
 end CHIP8_ALU;
 
 architecture RTL of CHIP8_ALU is
-
-    constant CHIP8_ALU_ADD: chip8_alu_ctrl_t := "000";
-    constant CHIP8_ALU_SUB: chip8_alu_ctrl_t := "001";
-    constant CHIP8_ALU_AND: chip8_alu_ctrl_t := "010";
-    constant CHIP8_ALU_OR:  chip8_alu_ctrl_t := "011";
-    constant CHIP8_ALU_XOR: chip8_alu_ctrl_t := "100";
-    constant CHIP8_ALU_SR:  chip8_alu_ctrl_t := "101";
-    constant CHIP8_ALU_SL:  chip8_alu_ctrl_t := "110";
-
     signal w_Result: std_logic_vector(CHIP8_WORD_SIZE downto 0);
+    signal w_Flag: std_logic;
 begin
 
     process(all) is
     begin
+        -- Default assignments
+        w_Result <= (others => '0');
+        w_Flag <= '0';
+
         case i_ALUCtrl is
-            when CHIP8_ALU_ADD =>
-                w_Result <= ('0' & i_VX) + ('0' & i_VY);
-            when CHIP8_ALU_SUB =>
-                w_Result <= ('0' & i_VX) - ('0' & i_VY);
-            when CHIP8_ALU_AND =>
+            when chip8_alu_add =>
+                w_Result <= std_logic_vector(unsigned('0' & i_VX) + unsigned('0' & i_VY));
+                w_Flag <= w_Result(CHIP8_WORD_SIZE); -- Carry flag
+            when chip8_alu_sub =>
+                w_Result <= std_logic_vector(unsigned('0' & i_VX) - unsigned('0' & i_VY));
+                w_Flag <= '1' when (unsigned(i_VX) >= unsigned(i_VY)) else '0';-- Set when no borrow occurs
+            when chip8_alu_and =>
                 w_Result <= '0' & (i_VX and i_VY);
-            when CHIP8_ALU_OR =>
+            when chip8_alu_or =>
                 w_Result <= '0' & (i_VX or i_VY);
-            when CHIP8_ALU_XOR =>
+            when chip8_alu_xor =>
                 w_Result <= '0' & (i_VX xor i_VY);
-            when CHIP8_ALU_SR =>
-                w_Result <= "00" & i_Vy(CHIP8_WORD_SIZE-1 downto 1);
-            when CHIP8_ALU_SL =>
-                w_Result <= '0' & i_Vy(CHIP8_WORD_SIZE-1 downto 1) & '0';
+            when chip8_alu_sr =>
+                w_Result <= std_logic_vector(shift_right(unsigned('0' & i_VY), 1));
+                w_Flag <= i_VY(0); -- Set the flag to the LSB prior to the shift
+            when chip8_alu_sl =>
+                w_Result <= std_logic_vector(shift_left(unsigned('0' & i_VY), 1));
+                w_Flag <= i_VY(CHIP8_WORD_SIZE-1); -- Set the flag to the MSB prior to the shift
             when others =>
                 w_Result <= (others => '0');
+                w_Flag <= '0';
         end case;
-
-        o_ALUData <= w_Result(CHIP8_WORD_SIZE-1 downto 0);
-        o_CB <= w_Result(CHIP8_WORD_SIZE);
-
     end process;
 
-end architecture;
+    o_ALUResult <= w_Result(CHIP8_WORD_SIZE-1 downto 0);
+    o_UniF <= w_Flag;
+
+end architecture RTL;
--- rtl/pkg/chip8_types.vhd
+++ rtl/pkg/chip8_types.vhd
@@ -4,10 +4,10 @@ use ieee.numeric_std.all;
 use work.chip8_const_pkg.all;
 
 package chip8_types_pkg is
-    
-    -- ALU File Types
-    subtype chip8_alu_ctrl_t is std_logic_vector(CHIP8_ALU_CTRL_SIZE-1 downto 0);
-
+    -- ALU Types
+    type chip8_alu_op_t is (chip8_alu_add, chip8_alu_sub, chip8_alu_and,
+                            chip8_alu_or, chip8_alu_xor, chip8_alu_sr,
+                            chip8_alu_sl);
     -- Register File Types
     type reg_arr_t is array (0 to REG_COUNT-1) of std_logic_vector(REG_DATA_WIDTH-1 downto 0);
 
