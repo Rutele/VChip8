2ab6a546f76757bcc37897dbf51503b397ddbe31
Michal Rucinski <rucinskimichal7@gmail.com>
2025-09-07 22:06:51 +0200

    Added Register Write and ALU operations

--- rtl/decoder/decoder.vhd
+++ rtl/decoder/decoder.vhd
@@ -5,16 +5,20 @@ use work.chip8_const_pkg.all;
 use work.chip8_types_pkg.all;
 
 entity InstructionDecoder is
+
     port(
         i_Clk : in std_logic;
         i_Rst : in std_logic;
         i_InstrOpcode : in chip8_instr_opcode_t;
+        i_InstrSub : in chip8_alu_op_t;
         o_PCWrite : out std_logic;
         o_IRWrite : out std_logic;
         o_RRWrite : out std_logic;
         o_RegFileWrite : out std_logic;
+        o_VFWrite : out std_logic;
         o_ALUOp : out chip8_alu_op_t
     );
+
 end InstructionDecoder;
 
 architecture RTL of InstructionDecoder is
@@ -35,9 +39,13 @@ begin
                     case i_InstrOpcode is
                         when chip8_instr_opcode_store_imm =>
                             r_CurrState <= chip8_fsm_state_writeVX;
+                        when chip8_instr_opcode_ALU_calc =>
+                            r_CurrState <= chip8_fsm_state_ALUOp;
                         when others =>
                             r_CurrState <= chip8_fsm_state_fetch;
                     end case;
+                when chip8_fsm_state_ALUOp =>
+                    r_CurrState <= chip8_fsm_state_writeVX;
                 when others =>
                     r_CurrState <= chip8_fsm_state_fetch;
             end case;
@@ -52,19 +60,36 @@ begin
                 o_IRWrite <= '1';
                 o_RRWrite <= '0';
                 o_RegFileWrite <= '0';
-                o_ALUOp <= chip8_alu_add;
+                o_VFWrite <= '0';
+                o_ALUOp <= chip8_alu_nop;
             when chip8_fsm_state_decode =>
                 o_PCWrite <= '0';
                 o_IRWrite <= '0';
                 o_RRWrite <= '1';
                 o_RegFileWrite <= '0';
-                o_ALUOp <= chip8_alu_add;
+                o_VFWrite <= '0';
+                o_ALUOp <= chip8_alu_nop;
+            when chip8_fsm_state_writeVX =>
+                o_PCWrite <= '0';
+                o_IRWrite <= '0';
+                o_RRWrite <= '1';
+                o_RegFileWrite <= '1';
+                o_VFWrite <= '0';
+                o_ALUOp <= chip8_alu_nop;
+            when chip8_fsm_state_ALUOp =>
+                o_PCWrite <= '0';
+                o_IRWrite <= '0';
+                o_RRWrite <= '1';
+                o_RegFileWrite <= '0';
+                o_VFWrite <= '0';
+                o_ALUOp <= i_InstrSub;
             when others =>
                 o_PCWrite <= '0';
                 o_IRWrite <= '0';
                 o_RRWrite <= '0';
                 o_RegFileWrite <= '0';
-                o_ALUOp <= chip8_alu_add;
+                o_VFWrite <= '0';
+                o_ALUOp <= chip8_alu_nop;
         end case;
     end process;
 
--- rtl/pkg/chip8_types.vhd
+++ rtl/pkg/chip8_types.vhd
@@ -5,18 +5,18 @@ use work.chip8_const_pkg.all;
 
 package chip8_types_pkg is
     -- ALU Types
-    type chip8_alu_op_t is (chip8_alu_add, chip8_alu_sub, chip8_alu_and,
-                            chip8_alu_or, chip8_alu_xor, chip8_alu_sr,
-                            chip8_alu_sl);
+    type chip8_alu_op_t is (chip8_alu_nop, chip8_alu_add, chip8_alu_sub,
+                            chip8_alu_and, chip8_alu_or, chip8_alu_xor,
+                            chip8_alu_sr, chip8_alu_sl);
     -- Register File Types
     type reg_arr_t is array (0 to REG_COUNT-1) of std_logic_vector(REG_DATA_WIDTH-1 downto 0);
 
     -- Instruction Decoder Types
-    type chip8_instr_opcode_t is (chip8_instr_opcode_store_imm);
+    type chip8_instr_opcode_t is (chip8_instr_opcode_store_imm, chip8_instr_opcode_ALU_calc);
 
     -- Instruction Decoder FSM States
     type chip8_fsm_state_t is (chip8_fsm_state_fetch, chip8_fsm_state_decode,
-                               chip8_fsm_state_writeVX);
+                               chip8_fsm_state_writeVX, chip8_fsm_state_ALUOp);
 
 end package;
 
