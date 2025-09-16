81c11e60320e2640b418d320b5040c12339e22f1
Rutele <rucinskimichal7@gmail.com>
2025-09-16 18:33:14 +0200

    Added test runner script

---
+++ test.py
@@ -0,0 +1,50 @@
+import argparse
+from tools import test_utils
+
+tests = {
+    "chip8_alu": {
+        "dut_path": "rtl/datapath/alu/alu.vhd",
+        "dependencies": ["rtl/pkg/chip8_const.vhd", "rtl/pkg/chip8_types.vhd"],
+        "test_module": "alu_test"
+    },
+    "registerfile": {
+        "dut_path": "rtl/datapath/register_file/regfile.vhd",
+        "dependencies": ["rtl/pkg/chip8_const.vhd", "rtl/pkg/chip8_types.vhd"],
+        "test_module": "regfile_test"
+    } 
+}
+
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dut', nargs="+")
+    parser.add_argument('--test-names', nargs="+")
+    return parser
+
+
+def check_duts(duts: list) -> list:
+    if duts is None:
+        return tests.keys()
+    else:
+        tested_duts = []
+        for dut in duts:
+            if dut in tests:
+                tested_duts.append(dut)
+            else:
+                raise ValueError(f"No tests defined for {dut} entity.")
+        return tested_duts
+
+
+def run_tests(args: argparse.Namespace):
+    duts = check_duts(args.dut)
+
+    for dut_name in duts:
+        dut_path = tests[dut_name]["dut_path"]
+        dut_deps = tests[dut_name]["dependencies"]
+        dut_test_module = tests[dut_name]["test_module"]
+        current_test = test_utils.DeviceTester(dut_name, dut_path, dut_deps, dut_test_module, args.test_names)
+        current_test.run_test()
+
+
+if __name__ == "__main__":
+    run_tests(create_parser().parse_args())
--- tools/test_utils.py
+++ tools/test_utils.py
@@ -4,15 +4,16 @@ from cocotb.runner import get_runner
 
 class DeviceTester():
 
-    def __init__(self, dut_name: str, dut_path: str, dut_deps: list[str], test_module: str, 
-                 sim_name: str = "ghdl", output_path: str = "test_out",):
+    def __init__(self, dut_name: str, dut_path: str, dut_deps: list[str], test_module: str,
+                 test_cases: str | list[str] | None, sim_name: str = "ghdl", output_path: str = "test_out",):
 
         self.dut_name = dut_name
         self.dut_path = Path(dut_path)
         self.dut_deps = [Path(path) for path in dut_deps]
         self.test_module = test_module
-        self.output_path = output_path
+        self.output_path = output_path + f"//{dut_name}"
         self.sim_name = sim_name
+        self.test_cases = test_cases
         self.test_runner = None
 
         self._validate_paths()
@@ -47,4 +48,5 @@ class DeviceTester():
 
     def run_test(self):
         self.test_runner.test(test_module=self.test_module, hdl_toplevel=self.dut_name, hdl_toplevel_library="work",
-                              test_args=["--std=08"], plusargs=[f"--wave={self.dut_name}_test_waveform.ghw"], waves=True,)
+                              test_args=["--std=08"], plusargs=[f"--wave={self.dut_name}_test_waveform.ghw"], waves=True,
+                              test_dir=self.output_path, build_dir=self.output_path, testcase=self.test_cases)
